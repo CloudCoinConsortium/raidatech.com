b9df77917dc55c4d32971ba8f2fdfc812953eeb3
Dipen Chauhan <get.dipen@gmail.com>
2019-03-20 01:10:58 -0700

    Initial commit

---
+++ app/controllers/solutions_controller.rb
@@ -0,0 +1,13 @@
+class SolutionsController < ApplicationController
+  def collectibles
+  end
+
+  def consumables
+  end
+
+  def digital_cash
+  end
+
+  def digital_securities
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'solutions/collectibles'
+  get 'solutions/consumables'
+  get 'solutions/digital_cash'
+  get 'solutions/digital_securities'
+  get 'welcome/index'
+  get 'welcome/faq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
