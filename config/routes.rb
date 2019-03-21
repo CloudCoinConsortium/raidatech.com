c74042d1809d1e7544622b4f028002450beedb84
Dipen Chauhan <get.dipen@gmail.com>
2019-03-21 11:01:05 -0700

    Using .html

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@
 #
 #                       Prefix Verb URI Pattern                                                                              Controller#Action
 #                         root GET  /                                                                                        welcome#index
-#       solutions_collectibles GET  /solutions/collectibles(.:format)                                                        solutions#collectibles
-#        solutions_consumables GET  /solutions/consumables(.:format)                                                         solutions#consumables
-#       solutions_digital_cash GET  /solutions/digital_cash(.:format)                                                        solutions#digital_cash
-# solutions_digital_securities GET  /solutions/digital_securities(.:format)                                                  solutions#digital_securities
-#                        index GET  /index(.:format)                                                                         welcome#index
-#                          faq GET  /faq(.:format)                                                                           welcome#faq
+#       solutions_collectibles GET  /solutions/collectibles.html(.:format)                                                   solutions#collectibles
+#        solutions_consumables GET  /solutions/consumables.html(.:format)                                                    solutions#consumables
+#       solutions_digital_cash GET  /solutions/digital_cash.html(.:format)                                                   solutions#digital_cash
+# solutions_digital_securities GET  /solutions/digital_securities.html(.:format)                                             solutions#digital_securities
+#                        index GET  /index.html(.:format)                                                                    welcome#index
+#                          faq GET  /faq.html(.:format)                                                                      welcome#faq
 #           rails_service_blob GET  /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
 #    rails_blob_representation GET  /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
 #           rails_disk_service GET  /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
@@ -16,12 +16,12 @@
 
 Rails.application.routes.draw do
   root "welcome#index"
-  get 'solutions/collectibles'
-  get 'solutions/consumables'
-  get 'solutions/digital_cash'
-  get 'solutions/digital_securities'
-  get 'index', to: "welcome#index"
-  get 'faq', to: "welcome#faq"
+  get 'solutions/collectibles.html', to: "solutions#collectibles", as: "solutions_collectibles"
+  get 'solutions/consumables.html', to: "solutions#consumables", as: "solutions_consumables"
+  get 'solutions/digital_cash.html', to: "solutions#digital_cash", as: "solutions_digital_cash"
+  get 'solutions/digital_securities.html', to: "solutions#digital_securities", as: "solutions_digital_securities"
+  get 'index.html', to: "welcome#index", as: "index"
+  get 'faq.html', to: "welcome#faq", as: "faq"
   # get 'welcome/faq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
