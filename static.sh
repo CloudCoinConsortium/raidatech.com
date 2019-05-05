b8ec4de539881ed2e3e8f8c1fb88bee5a5b39d55
Dipen Chauhan <get.dipen@gmail.com>
2019-05-05 16:19:15 -0700

    Using the rake method

--- static.sh
+++ static.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -x
-rails static:stop_rails_server
-rails static:stop_static_server
-rm -rf out/
-mkdir out
-rm -rf public/assets/
+# rails static:stop_rails_server
+# rails static:stop_static_server
+# rm -rf out/
+# mkdir out
+# rm -rf public/assets/
 
-rails assets:clean
-RAILS_ENV=static rails assets:precompile
+# rails assets:clean
+# RAILS_ENV=static rails assets:precompile
 
-# read -p "Press [Enter] key to continue..."
 
-RAILS_ENV=static rails s -p 3000 -d
+# RAILS_ENV=static rails s -p 3000 -d
 
-sleep 5s
+# sleep 5s
 
-cd out
-wget -mnH http://localhost:3000/
+# cd out
+# wget -mnH http://localhost:3000/
 
-# read -p "Press [Enter] key to continue..."
 
-cd ../
-rsync -ruv --exclude=.svn/ public/ out/
+# cd ../
+# rsync -ruv --exclude=.svn/ public/ out/
 
-cd out
-heel --daemonize --port 8000
+# cd out
+# heel --daemonize --port 8000
+
+RAILS_ENV=static rails static:generate2
